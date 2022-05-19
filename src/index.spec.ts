f7566dc100b9ab188db3bb80956327102d6d8e34
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-05-19 12:46:35 -0700

    Add normalize to buildUrl
    - trim function names

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,32 +1,45 @@
-import { validateDoi, buildDoiUrl, sanitizeDoi } from './';
+import { validate, buildUrl, normalize } from './'
 
 describe('validateDoi', () => {
   test('should return true if doi is correct', () => {
-    expect(validateDoi('10.1234/56789')).toBe(true);
-  });
+    expect(validate('10.1234/56789')).toBe(true)
+  })
 
   // can expand on here if needed
   test('should return false if doi is incorrect', () => {
-    expect(validateDoi(':10.1234/56789a')).toBe(false);
-  });
-});
+    expect(validate(':10.1234/56789a')).toBe(false)
+  })
+})
 
 describe('buildDoiUrl', () => {
   test('should build doi.org url with doi', () => {
-    expect(buildDoiUrl('10.1234/56789')).toBe('https://doi.org/10.1234/56789');
-  });
-});
+    expect(buildUrl('10.1234/56789')).toBe('https://doi.org/10.1234/56789')
+  })
 
-describe('sanitizeDoi', () => {
+  test.each([
+    ['http://dx.doi.org/10.1016/j.cageo.2015.09.015'],
+    ['http://www.doi.org/10.1016/j.cageo.2015.09.015'],
+    ['http://doi.org/10.1016/j.cageo.2015.09.015'],
+    ['doi.org/10.1016/j.cageo.2015.09.015'],
+  ])('should normalize url when it is %p', (doi) => {
+    expect(buildUrl(doi)).toBe('https://doi.org/10.1016/j.cageo.2015.09.015')
+  })
+})
+
+describe('normalize', () => {
   test('should remove "doi:"', () => {
-    expect(sanitizeDoi('doi:10.1234/56789')).toBe('10.1234/56789');
-  });
+    expect(normalize('doi:10.1234/56789')).toBe('10.1234/56789')
+  })
 
   test('should extract doi from doi.org url', () => {
-    expect(sanitizeDoi('doi.org/10.1234/56789')).toBe('10.1234/56789');
-  });
+    expect(normalize('doi.org/10.1234/56789')).toBe('10.1234/56789')
+  })
 
   test('should extract doi from doi.org url', () => {
-    expect(sanitizeDoi('https://doi.org/10.1234/56789')).toBe('10.1234/56789');
-  });
-});
+    expect(normalize('https://doi.org/10.1234/56789')).toBe('10.1234/56789')
+  })
+
+  test('should handle www.', () => {
+    expect(normalize('http://www.doi.org/10.1234/56789')).toBe('10.1234/56789')
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 // source: https://www.crossref.org/blog/dois-and-matching-regular-expressions/
-const DOI_VALIDATION_PATTERN = /^10.\d{4,9}\/[-._;()/:A-Z0-9]+$/i;
-const DOI_URL_PATTERN = /(?:https?:\/\/)?(?:dx\.)?doi.org\//;
+const DOI_VALIDATION_PATTERN = /^10.\d{4,9}\/[-._;()/:A-Z0-9]+$/i
+const DOI_URL_PATTERN = /(?:https?:\/\/)?(?:dx\.)?(?:www\.)?doi.org\//
 
-export function validateDoi(v: string) {
-  return v.match(DOI_VALIDATION_PATTERN) !== null;
+export function validate(v: string) {
+  return v.match(DOI_VALIDATION_PATTERN) !== null
 }
 
-export function buildDoiUrl(doi: string) {
-  return `https://doi.org/${doi}`;
-}
-
-export function sanitizeDoi(str: string) {
+export function normalize(str: string) {
   if (str.startsWith('doi:')) {
-    return str.substring(4);
+    return str.substring(4)
   }
-  return str.replace(DOI_URL_PATTERN, '');
+  return str.replace(DOI_URL_PATTERN, '')
+}
+
+export function buildUrl(doi: string) {
+  return `https://doi.org/${normalize(doi)}`
 }
